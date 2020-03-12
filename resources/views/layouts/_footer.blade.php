9d537b77b72f49d897dccad8cd699939648f649c
meow <liuguozhu720@gmail.com>
2020-03-12 12:58:55 +0800

    Separate default layout header and footer to subview

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png"
+   alt="image" class="brand-icon">
+  <a href="#" target="_blank">
+    Practice makes perfect.
+  </a>
+
+  <div class="float-right">
+    <a href="/about">About</a>
+  </div>
+</footer>
