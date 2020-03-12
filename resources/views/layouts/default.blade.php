dff6fa18fdb70d40ec29b3e15f2fce37f04a8dea
meow <liuguozhu720@gmail.com>
2020-03-12 12:42:47 +0800

    Optimize style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,22 @@
 <html>
 <head>
   <title>@yield('title', 'Weibo App')</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-  @yield('content')
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">Weibo App</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a href="/help" class="nav-link">Help</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-  <h1>home</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      You are looking <a href="https://lgzzzz.com">Meow's Weibo</a> project home page.
+    </p>
+    <p>
+      Everything starts from here.
+    </p>
+    <p>
+      <a href="#" class="btn btn-lg btn-success" role="button">Register now</a>
+    </p>
+  </div>
 @stop
