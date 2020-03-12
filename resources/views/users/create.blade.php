7d046237608539b2343a51f2935d818af589c6a9
meow <liuguozhu720@gmail.com>
2020-03-12 12:27:06 +0000

    User sign up form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,33 @@
 @section('title', 'Sign up')
 
 @section('content')
-<h1>Sign up</h1>
+<div class="offset-md-2 col-md-8">
+  <div class="card">
+    <div class="card-header">
+      <h5>Sign up</h5>
+    </div>
+    <div class="card-body">
+      <form action="{{ route('users.store') }}" method="POST">
+        <div class="form-group">
+          <label for="name">Name: </label>
+          <input class="form-control" type="text" name="name" value="{{ old('name') }}">
+        </div>
+        <div class="form-group">
+          <label for="name">Email: </label>
+          <input class="form-control" type="email" name="email" value="{{ old('email') }}">
+        </div>
+        <div class="form-group">
+          <label for="name">Password: </label>
+          <input class="form-control" type="password" name="password" value="{{ old('password') }}">
+        </div>
+        <div class="form-group">
+          <label for="password_confirmation">Name: </label>
+          <input class="form-control" type="text" name="password_confirmation" value="{{ old('password_confirmation') }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Sign up</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
