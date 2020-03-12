3900338ee91f2c63547da2379ce44544775b53d5
meow <liuguozhu720@gmail.com>
2020-03-12 12:36:23 +0000

    Add CSRF verification

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,6 +9,7 @@
     </div>
     <div class="card-body">
       <form action="{{ route('users.store') }}" method="POST">
+        @csrf
         <div class="form-group">
           <label for="name">Name: </label>
           <input class="form-control" type="text" name="name" value="{{ old('name') }}">
